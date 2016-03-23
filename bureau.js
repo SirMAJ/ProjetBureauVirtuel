fda568ce37ffd16fe0d9511a3a650703b543e14c
NOVEL Mathias <novelm@hostname.iut2.upmf-grenoble.fr>
2016-03-23 08:41:55 +0100

    Croix qui ferme

--- bureau.js
+++ bureau.js
@@ -1,3 +1,26 @@
+console.log("Bureau.js");
 $( ".selector" ).accordion({
   heightStyle: "fill"
 });
+
+function init() {
+	//Accordéon
+	console.log("Init");
+	$(function() {
+		$( "#accordion" ).accordion();
+	});
+	///////////////////////////////////
+
+	//Onglets
+	$(function() {
+		var tabs = $( "#tabs" ).tabs();
+		
+		tabs.delegate( "span.ui-icon-close", "click", function() {
+			var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
+			$( "#" + panelId ).remove();
+			tabs.tabs( "refresh" );
+		});
+	});
+
+
+}
