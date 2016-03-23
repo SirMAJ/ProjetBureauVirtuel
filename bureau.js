f6948e4bf37683bfe68d0da89a3c22111552517d
NOVEL Mathias <novelm@hostname.iut2.upmf-grenoble.fr>
2016-03-23 09:56:40 +0100

    ee

--- bureau.js
+++ bureau.js
@@ -4,23 +4,52 @@ $( ".selector" ).accordion({
 });
 
 function init() {
-	//Accordéon
 	console.log("Init");
+
+	/////////////////////
+	//		Accordéon		/////////////////
 	$(function() {
 		$( "#accordion" ).accordion();
 	});
 	///////////////////////////////////
 
-	//Onglets
+	///////////////////
+	//		Onglets		///////////////////////////////////////////////////////////////////
 	$(function() {
 		var tabs = $( "#tabs" ).tabs();
-		
+		tabTemplate = "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close'>Remove Tab</span></li>";
+		tabCounter = 4;
+		tabtextCounter = 0;
+
+		//Methode pour pouvoir fermer les onglets
 		tabs.delegate( "span.ui-icon-close", "click", function() {
 			var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
 			$( "#" + panelId ).remove();
 			tabs.tabs( "refresh" );
 		});
-	});
 
+		//Création d'un bouton ajouter fichier texte
+		$("#body").append("<button id=\"add_tab\">Add Tab</button>");
+		$("#add_tab").button().click(function() {addTab();});
+
+		//Méthode executé lors du clique sur créer fichier texte
+		function addTab() {
+			var label = "Tab " + tabCounter,
+			id = "tabs-" + tabCounter,
+			li = $( tabTemplate.replace( /#\{href\}/g, "#" + id ).replace( /#\{label\}/g, label ) );
+
+			tabs.find( ".ui-tabs-nav" ).append( li );
+			id = "TextBox-" + tabtextCounter;
+			tabs.append( "<div id='" + id + "' contenteditable=\"true\"><p>" + /*DOC TEXT*/ + "</p></div>" );
+			tabs.tabs( "refresh" );
+			tabCounter++;
+			tabtextCounter++;
+		}
+	});
+	///////////////////////////////////////////////////////////////////////////////////
+
+}
+
+function DocText() {
 
 }
